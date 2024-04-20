17f6190abe8ea8fcdebb31c8ce640efddffdec5d
NaeemRaza16 <naeemraza2916@gmail.com>
2024-04-20 12:36:05 +0500

    upload all files to github

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <Button variant={"destructive"} size='sm'>ShadCN Button</Button>
+    </main>
+  );
+};
+
+export default page;
